44b859ee8c6d3fd53be188741122005fa5e9dad0
Priya <priya_93_x@hotmail.co.uk>
2024-11-18 10:15:52 +0000

    add map source and layer

--- ui/components/Base/Mapbox/Map.vue
+++ ui/components/Base/Mapbox/Map.vue
@@ -40,6 +40,23 @@ export default {
 
     this.map.on('load', () => {
       this.loaded = true
+
+      this.map.addSource('ArcGIS-dataset', {
+        type: 'geojson',
+        data: 'https://services3.arcgis.com/fp1tibNcN9mbExhG/arcgis/rest/services/Fiji_Sponges_Algae/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
+      })
+
+      this.map.addLayer({
+        id: 'arcGIS-layer',
+        type: 'circle',
+        source: 'ArcGIS-dataset',
+        paint: {
+          'circle-color': '#4264fb',
+          'circle-radius': 8,
+          'circle-stroke-width': 2,
+          'circle-stroke-color': '#ffffff'
+        }
+      })
     })
   }
 }
